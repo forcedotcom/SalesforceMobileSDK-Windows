8b4e0d1b9334892e4ead81bd6ccde5ef25784293
Wolfgang Mathurin <wmathurin@salesforce.com>
2013-01-24 16:01:02 -0800

    RestExplorer phone app (skeleton) / PlatformAdapter class that uses reflection to pull concrete implementation from platform specific assembly

--- SalesforceSDK/Sample.RestExplorer.Phone/Properties/AssemblyInfo.cs
+++ SalesforceSDK/Sample.RestExplorer.Phone/Properties/AssemblyInfo.cs
@@ -1,18 +1,27 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SalesforceSDK.Core.UnitTests")]
+[assembly: AssemblyTitle("Sample.RestExplorer.Phone")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SalesforceSDK.Core.UnitTests")]
+[assembly: AssemblyProduct("Sample.RestExplorer.Phone")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1d165641-0174-4cdd-8a8f-170c15650470")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -21,8 +30,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
