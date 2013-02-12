dc114fe2951fadfa8c43eeeadda324bc589bbd04
Wolfgang Mathurin <wmathurin@salesforce.com>
2013-02-12 11:23:38 -0800

    Moving non-view code from RestActionPage to RestActionViewModel

--- SalesforceSDK/Salesforce.Sample.RestExplorer.Phone/Pages/RestActionPage.xaml.cs
+++ SalesforceSDK/Salesforce.Sample.RestExplorer.Phone/Pages/RestActionPage.xaml.cs
@@ -1,34 +1,44 @@
 ﻿using Microsoft.Phone.Controls;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using Salesforce.SDK.Net;
+using Salesforce.Sample.RestExplorer.ViewModels;
 using Salesforce.SDK.Rest;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
 using System.Windows.Navigation;
 
 namespace Salesforce.Sample.RestExplorer.Phone
 {
     public partial class RestActionPage : PhoneApplicationPage
     {
+        private RestActionViewModel _viewModel;
         private RestAction _restAction;
 
         public RestActionPage()
         {
             InitializeComponent();
-            ShowResults(null);
-            btnAction.Click += OnActionClicked;
+            ShowResponse(null);
+            _viewModel = DataContext as RestActionViewModel;
+            _viewModel.PropertyChanged += OnViewModelPropertyChanged;
+        }
+
+        private void OnViewModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == RestActionViewModel.RETURNED_REST_RESPONSE)
+            {
+                // Data binding would be more elegant
+                Dispatcher.BeginInvoke(() => { ShowResponse(_viewModel.ReturnedRestResponse); });
+            }
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
             IDictionary<String, String> qs = NavigationContext.QueryString;
-            _restAction = (RestAction) Enum.Parse(typeof(RestAction), qs["rest_action"]);
+            _restAction = (RestAction)Enum.Parse(typeof(RestAction), qs["rest_action"]);
+            _viewModel.SelectedRestAction = _restAction;
 
             SetupButtons();
         }
@@ -106,23 +116,15 @@ namespace Salesforce.Sample.RestExplorer.Phone
             }
         }
 
-        private void OnActionClicked(Object sender, RoutedEventArgs e)
-        {
-            ClientManager cm = new ClientManager(Config.LoginOptions);
-            RestClient rc = cm.GetRestClient();
-            if (rc != null)
-            {
-                RestRequest request = BuildRestRequest();
-                rc.SendAsync(request, (response) => Dispatcher.BeginInvoke(() => { ShowResponse(response); }));
-            }
-        }
 
         private void ShowResponse(RestResponse response)
         {
-            ShowResults(new String[] { "<b>StatusCode:</b>" + response.StatusCode, "<b>Body:</b>\n" + response.PrettyBody });
+            wbResult.NavigateToString(BuildHtml(response == null 
+                ? null 
+                : new String[] { "<b>Status Code:</b>" + response.StatusCode, "<b>Body:</b>\n" + response.PrettyBody}));
         }
 
-        private void ShowResults(String[] blocks)
+        private string BuildHtml(String[] blocks)
         {
             String htmlHead = @"
             <head>
@@ -146,73 +148,8 @@ namespace Salesforce.Sample.RestExplorer.Phone
                 }
             }
             sb.Append("</body>");
-            wbResult.NavigateToString(sb.ToString());
-        }
 
-        private RestRequest BuildRestRequest()
-        {
-            switch (_restAction)
-            {
-                case RestAction.VERSIONS:
-                    return RestRequest.GetRequestForVersions();
-                case RestAction.RESOURCES:
-                    return RestRequest.GetRequestForResources(tbApiVersion.Text);
-                case RestAction.DESCRIBE_GLOBAL:
-                    return RestRequest.GetRequestForDescribeGlobal(tbApiVersion.Text);
-                case RestAction.METADATA:
-                    return RestRequest.GetRequestForMetadata(tbApiVersion.Text, tbObjectType.Text);
-                case RestAction.DESCRIBE:
-                    return RestRequest.GetRequestForDescribe(tbApiVersion.Text, tbObjectType.Text);
-                case RestAction.CREATE:
-                    return RestRequest.GetRequestForCreate(tbApiVersion.Text, tbObjectType.Text, ParseFieldsValue());
-                case RestAction.RETRIEVE:
-                    return RestRequest.GetRequestForRetrieve(tbApiVersion.Text, tbObjectType.Text, tbObjectId.Text, ParseFieldListValue());
-                case RestAction.UPSERT:
-                    return RestRequest.GetRequestForUpsert(tbApiVersion.Text, tbObjectType.Text, tbExternalIdField.Text, tbExternalId.Text, ParseFieldsValue());
-                case RestAction.UPDATE:
-                    return RestRequest.GetRequestForUpdate(tbApiVersion.Text, tbObjectType.Text, tbObjectId.Text, ParseFieldsValue());
-                case RestAction.DELETE:
-                    return RestRequest.GetRequestForDelete(tbApiVersion.Text, tbObjectType.Text, tbObjectId.Text);
-                case RestAction.QUERY:
-                    return RestRequest.GetRequestForQuery(tbApiVersion.Text, tbSoql.Text);
-                case RestAction.SEARCH:
-                    return RestRequest.GetRequestForSearch(tbApiVersion.Text, tbSosl.Text);
-                case RestAction.MANUAL:
-                    return BuildManualRestReuqest();
-                default:
-                    throw new InvalidOperationException();
-            }
+            return sb.ToString();
         }
-
-        private RestRequest BuildManualRestReuqest()
-        {
-            RestMethod method = RestMethod.GET;
-            foreach (var child in spMethods.Children)
-            {
-                if (child.GetType() == typeof(RadioButton) && ((RadioButton)child).IsChecked == true)
-                {
-                    method = (RestMethod)Enum.Parse(typeof(RestMethod), ((RadioButton)child).Tag.ToString(), true);
-                    break;
-                }
-            }
-            return new RestRequest(method, tbRequestPath.Text, tbRequestBody.Text, ContentType.JSON);
-        }
-
-        private string[] ParseFieldListValue()
-        {
-            return tbFieldList.Text.Split(',');
-        }
-
-        private Dictionary<String, Object> ParseFieldsValue()
-        {
-            Dictionary<String, Object> result = new Dictionary<String, Object>();
-            JObject fieldMap = JObject.Parse(tbFields.Text);
-            foreach (var item in fieldMap)
-            {
-                result.Add(item.Key, item.Value);
-            }
-            return result;
-        }
-
     }
 }
---
+++ SalesforceSDK/Salesforce.Sample.RestExplorer.Phone/ViewModels/RestActionViewModel.cs
@@ -0,0 +1,214 @@
+﻿using Newtonsoft.Json.Linq;
+using Salesforce.Sample.RestExplorer.Phone;
+using Salesforce.SDK.Net;
+using Salesforce.SDK.Rest;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Salesforce.Sample.RestExplorer.ViewModels
+{
+    public class RestActionViewModel : INotifyPropertyChanged
+    {
+        // Bound properties
+        public const String SELECTED_REST_ACTION = "SelectedRestAction";
+        public const String RETURNED_REST_RESPONSE = "ReturnedRestResponse";
+        public const String API_VERSION = "ApiVersion";
+        public const String OBJECT_TYPE = "ObjectType";
+        public const String OBJECT_ID = "ObjectId";
+        public const String EXTERNAL_ID_FIELD = "ExternalIdField";
+        public const String EXTERNAL_ID = "ExternalId";
+        public const String FIELD_LIST = "FieldList";
+        public const String FIELDS = "Fields";
+        public const String SOQL = "Soql";
+        public const String SOSL = "Sosl";
+        public const String REQUEST_PATH = "RequestPath";
+        public const String REQUEST_BODY = "RequestBody";
+
+        private RestAction _restAction;
+        public RestAction SelectedRestAction
+        {
+            get
+            {
+                return _restAction;
+            }
+
+            set
+            {
+                _restAction = value;
+            }
+        }
+
+        private SendRequestCommand _sendRequestCommand;
+        public SendRequestCommand SendRequest
+        {
+            get
+            {
+                if (_sendRequestCommand == null)
+                {
+                    _sendRequestCommand = new SendRequestCommand(this);
+                }
+                return _sendRequestCommand;
+            }
+        }
+        
+        private Dictionary<String, String> _properties;
+        public String this[String name]
+        {
+            get
+            {
+                return _properties[name];
+            }
+
+            set
+            {
+                _properties[name] = value;
+                RaisePropertyChanged(name);
+            }
+        }
+
+        private RestResponse _returnedRestResponse;
+        public RestResponse ReturnedRestResponse
+        {
+            get
+            {
+                return _returnedRestResponse;
+            }
+
+            set
+            {
+                _returnedRestResponse = value;
+                RaisePropertyChanged(RETURNED_REST_RESPONSE);
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string p)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(p));
+            }        
+        }
+
+        public RestActionViewModel()
+        {
+            _properties = new Dictionary<string, string>()
+            {
+                {API_VERSION, "v26.0"},
+                {OBJECT_TYPE, "Account"},
+                {OBJECT_ID, "Id"},
+                {EXTERNAL_ID_FIELD, "ExternalIdField"},
+                {EXTERNAL_ID, "ExternalId"},
+                {FIELD_LIST, "Id,Name"},
+                {FIELDS, "{\"Name\":\"acme\"}"},
+                {SOQL, "SELECT Id,Name FROM Account"},
+                {SOSL, "FIND {acme*}"},
+                {REQUEST_PATH, "/services/data/v26.0/chatter/feeds/news/me"},
+                {REQUEST_BODY, "Body"}
+            };
+        }
+    }
+
+    public class SendRequestCommand : ICommand
+    {
+        private RestActionViewModel _vm;
+
+        public SendRequestCommand(RestActionViewModel vm)
+        {
+            _vm = vm;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(Object parameter)
+        {
+            return true; // server-side validation only
+        }
+
+        public void Execute(Object parameter)
+        {
+            ClientManager cm = new ClientManager(Config.LoginOptions);
+            RestClient rc = cm.GetRestClient();
+            if (rc != null)
+            {
+                RestRequest request = BuildRestRequest();
+                rc.SendAsync(request, (response) => { _vm.ReturnedRestResponse = response; });
+            }
+        }
+
+        private RestRequest BuildRestRequest()
+        {
+            switch (_vm.SelectedRestAction)
+            {
+                case RestAction.VERSIONS:
+                    return RestRequest.GetRequestForVersions();
+                case RestAction.RESOURCES:
+                    return RestRequest.GetRequestForResources(_vm[RestActionViewModel.API_VERSION]);
+                case RestAction.DESCRIBE_GLOBAL:
+                    return RestRequest.GetRequestForDescribeGlobal(_vm[RestActionViewModel.API_VERSION]);
+                case RestAction.METADATA:
+                    return RestRequest.GetRequestForMetadata(_vm[RestActionViewModel.API_VERSION], _vm[RestActionViewModel.OBJECT_TYPE]);
+                case RestAction.DESCRIBE:
+                    return RestRequest.GetRequestForDescribe(_vm[RestActionViewModel.API_VERSION], _vm[RestActionViewModel.OBJECT_TYPE]);
+                case RestAction.CREATE:
+                    return RestRequest.GetRequestForCreate(_vm[RestActionViewModel.API_VERSION], _vm[RestActionViewModel.OBJECT_TYPE], ParseFieldsValue());
+                case RestAction.RETRIEVE:
+                    return RestRequest.GetRequestForRetrieve(_vm[RestActionViewModel.API_VERSION], _vm[RestActionViewModel.OBJECT_TYPE], _vm[RestActionViewModel.OBJECT_ID], ParseFieldListValue());
+                case RestAction.UPSERT:
+                    return RestRequest.GetRequestForUpsert(_vm[RestActionViewModel.API_VERSION], _vm[RestActionViewModel.OBJECT_TYPE], _vm[RestActionViewModel.EXTERNAL_ID_FIELD], _vm[RestActionViewModel.EXTERNAL_ID], ParseFieldsValue());
+                case RestAction.UPDATE:
+                    return RestRequest.GetRequestForUpdate(_vm[RestActionViewModel.API_VERSION], _vm[RestActionViewModel.OBJECT_TYPE], _vm[RestActionViewModel.OBJECT_ID], ParseFieldsValue());
+                case RestAction.DELETE:
+                    return RestRequest.GetRequestForDelete(_vm[RestActionViewModel.API_VERSION], _vm[RestActionViewModel.OBJECT_TYPE], _vm[RestActionViewModel.OBJECT_ID]);
+                case RestAction.QUERY:
+                    return RestRequest.GetRequestForQuery(_vm[RestActionViewModel.API_VERSION], _vm[RestActionViewModel.SOQL]);
+                case RestAction.SEARCH:
+                    return RestRequest.GetRequestForSearch(_vm[RestActionViewModel.API_VERSION], _vm[RestActionViewModel.SOSL]);
+                case RestAction.MANUAL:
+                    return BuildManualRestReuqest();
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+
+        private RestRequest BuildManualRestReuqest()
+        {
+            /*
+            RestMethod method = RestMethod.GET;
+            foreach (var child in spMethods.Children)
+            {
+                if (child.GetType() == typeof(RadioButton) && ((RadioButton)child).IsChecked == true)
+                {
+                    method = (RestMethod)Enum.Parse(typeof(RestMethod), ((RadioButton)child).Tag.ToString(), true);
+                }
+            }
+            return new RestRequest(method, tbRequestPath.Text, tbRequestBody.Text, ContentType.JSON);
+             */
+            return null;
+        }
+
+        private string[] ParseFieldListValue()
+        {
+            return _vm[RestActionViewModel.FIELD_LIST].Split(',');
+        }
+
+        private Dictionary<String, Object> ParseFieldsValue()
+        {
+            Dictionary<String, Object> result = new Dictionary<String, Object>();
+            JObject fieldMap = JObject.Parse(_vm[RestActionViewModel.FIELDS]);
+            foreach (var item in fieldMap)
+            {
+                result.Add(item.Key, item.Value);
+            }
+            return result;
+        }
+
+    }
+}
