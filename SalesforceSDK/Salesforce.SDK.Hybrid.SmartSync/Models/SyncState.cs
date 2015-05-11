ee4ef84254d6b5afa5a1286bda36f497632ecc35
Kunal Chitalia <kchitalia@salesforce.com>
2015-05-11 09:08:44 -0700

    Add Hybrid SmartStore and SmartSync

---
+++ SalesforceSDK/Salesforce.SDK.Hybrid.SmartSync/Models/SyncState.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Salesforce.SDK.Auth;
+using Salesforce.SDK.SmartSync.Model;
+using Salesforce.SDK.SmartSync.Util;
+
+namespace Salesforce.SDK.Hybrid.SmartSync.Models
+{
+    public sealed class SyncState
+    {
+        private SDK.SmartSync.Model.SyncState NativeSyncState
+        {
+            get { return new SDK.SmartSync.Model.SyncState(); }
+        }
+
+        public MergeModeOptions MergeMode
+        {
+            get
+            {
+                var mode = NativeSyncState.MergeMode;
+                MergeModeOptions mergeMode;
+                Enum.TryParse(mode.ToString(), out mergeMode);
+                return mergeMode;
+            }
+        }
+
+        public static void SetupSyncsSoupIfNeeded(SmartStore.SmartStore store)
+        {
+            var nativeStore = JsonConvert.SerializeObject(store);
+            SDK.SmartSync.Model.SyncState.SetupSyncsSoupIfNeeded(JsonConvert.DeserializeObject<SDK.SmartStore.Store.SmartStore>(nativeStore));
+        }
+
+        public static SyncState CreateSyncDown(SmartStore.SmartStore store, string target,
+            string soupName, SyncOptions options)
+        {
+            var nativeStore = JsonConvert.SerializeObject(store);
+            var jObject = JsonConvert.DeserializeObject<SDK.SmartSync.Model.SyncDownTarget>(target);
+            var nativeOptions = JsonConvert.SerializeObject(options);
+            var state = SDK.SmartSync.Model.SyncState.CreateSyncDown(
+                JsonConvert.DeserializeObject<SDK.SmartStore.Store.SmartStore>(nativeStore), jObject, soupName, JsonConvert.DeserializeObject<SDK.SmartSync.Model.SyncOptions>(nativeOptions));
+            var syncState = JsonConvert.SerializeObject(state);
+            return JsonConvert.DeserializeObject<SyncState>(syncState);
+        }
+
+        public static SyncState CreateSyncUp(SmartStore.SmartStore store, SyncUpTarget target, SyncOptions options,
+            string soupName)
+        {
+            var nativeStore = JsonConvert.SerializeObject(store);
+            var syncUpTarget = JsonConvert.SerializeObject(target);
+            var nativeOptions = JsonConvert.SerializeObject(options);
+            var state =
+                SDK.SmartSync.Model.SyncState.CreateSyncUp(
+                    JsonConvert.DeserializeObject<SDK.SmartStore.Store.SmartStore>(nativeStore),
+                    JsonConvert.DeserializeObject<SDK.SmartSync.Model.SyncUpTarget>(syncUpTarget),
+                    JsonConvert.DeserializeObject<SDK.SmartSync.Model.SyncOptions>(nativeOptions), soupName);
+            var syncState = JsonConvert.SerializeObject(state);
+            return JsonConvert.DeserializeObject<SyncState>(syncState);
+        }
+
+        public static SyncState FromJson(string sync)
+        {
+            var jObject = JsonConvert.DeserializeObject<JObject>(sync);
+            var state = SDK.SmartSync.Model.SyncState.FromJson(jObject);
+            var syncState = JsonConvert.SerializeObject(state);
+            return JsonConvert.DeserializeObject<SyncState>(syncState);
+        }
+
+        public static SyncState ById(SmartStore.SmartStore store, long id)
+        {
+            var nativeStore = JsonConvert.SerializeObject(store);
+            var state = SDK.SmartSync.Model.SyncState.ById(
+                JsonConvert.DeserializeObject<SDK.SmartStore.Store.SmartStore>(nativeStore), id);
+            var nativeState = JsonConvert.SerializeObject(state);
+            return JsonConvert.DeserializeObject<SyncState>(nativeState);
+        }
+
+        public string AsJson()
+        {
+            var jObject = NativeSyncState.AsJson();
+            return JsonConvert.SerializeObject(jObject);
+        }
+
+        public void Save(SmartStore.SmartStore store)
+        {
+            var nativeStore = JsonConvert.SerializeObject(store);
+            NativeSyncState.Save(JsonConvert.DeserializeObject<SDK.SmartStore.Store.SmartStore>(nativeStore));
+        }
+    }
+
+    public enum MergeModeOptions
+    {
+        None,
+        Overwrite,
+        LeaveIfChanged
+    }
+
+    public enum SyncStatusTypes
+    {
+        New,
+        Running,
+        Done,
+        Failed
+    }
+
+    public enum SyncTypes
+    {
+        SyncDown,
+        SyncUp
+    }
+}
---
+++ SalesforceSDK/Salesforce.SDK.Hybrid.SmartSync/Models/SyncUpTarget.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Salesforce.SDK.Hybrid.SmartSync.Models
+{
+    public sealed class SyncUpTarget
+    {
+         private SDK.SmartSync.Model.SyncUpTarget _syncUpTarget;
+
+        public SyncUpTarget(string target)
+        {
+            _syncUpTarget = new SDK.SmartSync.Model.SyncUpTarget(JsonConvert.DeserializeObject<JObject>(target));
+        }
+
+        public static SyncUpTarget FromJSON(string target)
+        {
+            var nativeSyncUpTarget = SDK.SmartSync.Model.SyncUpTarget.FromJSON(JsonConvert.DeserializeObject<JObject>(target));
+            var syncUpTarget = JsonConvert.SerializeObject(nativeSyncUpTarget);
+            return JsonConvert.DeserializeObject<SyncUpTarget>(syncUpTarget);
+        }
+
+        public IAsyncOperation<String> CreateOnServerAsync(SyncManager syncManager, String objectType,
+            IDictionary<String, Object> fields)
+        {
+            var manager = JsonConvert.SerializeObject(syncManager);
+            var fieldList = new Dictionary<String, Object>(fields);
+            return Task.Run(async() => await _syncUpTarget.CreateOnServerAsync(JsonConvert.DeserializeObject<SDK.SmartSync.Manager.SyncManager>(manager), objectType, fieldList)).AsAsyncOperation();
+        }
+
+        public IAsyncOperation<bool> DeleteOnServer(SyncManager syncManager, String objectType, String objectId)
+        {
+            var manager = JsonConvert.SerializeObject(syncManager);
+            return
+                Task.Run(
+                    async () =>
+                        await
+                            _syncUpTarget.DeleteOnServer(
+                                JsonConvert.DeserializeObject<SDK.SmartSync.Manager.SyncManager>(manager), objectType,
+                                objectId)).AsAsyncOperation();
+        }
+
+        public IAsyncOperation<bool> UpdateOnServer(SyncManager syncManager, String objectType, String objectId,
+            IDictionary<String, Object> fields)
+        {
+            var manager = JsonConvert.SerializeObject(syncManager);
+            var fieldList = new Dictionary<String, Object>(fields);
+            return
+                Task.Run(
+                    async () =>
+                        await
+                            _syncUpTarget.UpdateOnServer(
+                                JsonConvert.DeserializeObject<SDK.SmartSync.Manager.SyncManager>(manager), objectType,
+                                objectId, fieldList)).AsAsyncOperation();
+        }
+
+        public IAsyncOperation<String> FetchLastModifiedDate(SyncManager syncManager, String objectType, String objectId)
+        {
+            var manager = JsonConvert.SerializeObject(syncManager);
+            return
+                Task.Run(
+                    async () =>
+                        await
+                            _syncUpTarget.FetchLastModifiedDate(
+                                JsonConvert.DeserializeObject<SDK.SmartSync.Manager.SyncManager>(manager), objectType,
+                                objectId)).AsAsyncOperation();
+        }
+
+        public object GetIdsOfRecordsToSyncUp(SyncManager syncManager, String soupName)
+        {
+            var manager = JsonConvert.SerializeObject(syncManager);
+            return _syncUpTarget.GetIdsOfRecordsToSyncUp(
+                JsonConvert.DeserializeObject<SDK.SmartSync.Manager.SyncManager>(manager), soupName);
+        }
+    }
+}
