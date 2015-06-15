ce179566e3b459e3b6b79812cb0c3e5e2bc26b91
Sean McNulty <smcnulty@salesforce.com>
2015-06-15 11:19:46 -0700

    Adding the samples to the sample repo.

---
+++ Samples/Salesforce.Sample.RestExplorer.Phone/LocalizedStrings.cs
@@ -0,0 +1,17 @@
+﻿using Salesforce.Sample.RestExplorer.Phone.Resources;
+
+namespace Salesforce.Sample.RestExplorer.Phone
+{
+    /// <summary>
+    ///     Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static readonly AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources
+        {
+            get { return _localizedResources; }
+        }
+    }
+}
