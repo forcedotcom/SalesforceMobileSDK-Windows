07131e9c78aef9ab8dfc5550c93d18169103226b
Sean McNulty <smcnulty@salesforce.com>
2015-06-17 15:45:19 -0700

    FIxing up hybrid.smartstore
    1) Fix issue of hybrid.smartstore using a bad instance of the native
    smartstore
    2) removed extra calls to createmetatable & serialization
    3) All "smartstore" hybrid methods return json string instead of object

--- SalesforceSDK/Hybrid.SmartStore/Store/ISmartStore.cs
+++ SalesforceSDK/Hybrid.SmartStore/Store/ISmartStore.cs
@@ -1,18 +1,18 @@
 ﻿using System.Runtime.InteropServices.WindowsRuntime;
 
-namespace Salesforce.SDK.Hybrid.SmartStore.Source.Store
+namespace Salesforce.SDK.Hybrid.SmartStore.Store
 {
     public interface ISmartStore
     {
         long CountQuery(QuerySpec querySpec);
-        object Query(QuerySpec querySpec, int pageIndex);
+        string Query(QuerySpec querySpec, int pageIndex);
         bool Delete(string soupName, [ReadOnlyArray()]long[] soupEntryIds, bool handleTx);
-        object Retrieve(string soupName, [ReadOnlyArray()]long[] soupEntryIds);
+        string Retrieve(string soupName, [ReadOnlyArray()]long[] soupEntryIds);
         bool HasSoup(string soupName);
         void RegisterSoup(string soupName, [ReadOnlyArray()]IndexSpec[] indexSpecs);
         void DropSoup(string soupName);
         void ResetDatabase();
-        object Upsert(string soupName, string soupElt, string externalIdPath, bool handleTx);
+        string Upsert(string soupName, string soupElt, string externalIdPath, bool handleTx);
         bool BeginDatabaseTransaction();
         bool CommitDatabaseTransaction();
         string ConvertSmartSql(string smartSql);
