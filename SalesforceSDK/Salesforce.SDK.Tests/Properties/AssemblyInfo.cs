f23a32d2d58139fa66f2e04bd9418c72f29e38ba
Sean McNulty <smcnulty@salesforce.com>
2015-07-02 12:14:27 -0700

    unit tests migrated

--- SalesforceSDK/Salesforce.SDK.Tests/Properties/AssemblyInfo.cs
+++ SalesforceSDK/Salesforce.SDK.Tests/Properties/AssemblyInfo.cs
@@ -1,17 +1,19 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("Salesforce.SDK.Store.Test")]
+[assembly: AssemblyTitle("Salesforce.SDK.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Salesforce.SDK.Store.Test")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("Salesforce")]
+[assembly: AssemblyProduct("Salesforce.SDK.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyMetadata("TargetPlatform", "UAP")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -23,6 +25,6 @@
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
