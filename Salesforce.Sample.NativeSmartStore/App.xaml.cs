dbd319ce31e5e3f62fd8dea5ddae2b23abe84bb8
Kunal Chitalia <kchitalia@salesforce.com>
2015-07-09 14:29:24 -0700

    Make sample apps work with latest Windows 10 SDK

---
+++ Salesforce.Sample.NativeSmartStore/App.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Salesforce.Sample.NativeSmartStoreSample.Shared.Pages;
+using Salesforce.SDK.App;
+using Salesforce.SDK.Auth;
+using Salesforce.SDK.Source.Security;
+using System;
+using Windows.UI.Xaml.Navigation;
+using Salesforce.Sample.NativeSmartStore.Settings;
+
+// The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=402347&clcid=0x409
+
+namespace Salesforce.Sample.NativeSmartStore
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : SalesforceApplication
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+            : base()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when Navigation to a certain page fails
+        /// </summary>
+        /// <param name="sender">The Frame which failed navigation</param>
+        /// <param name="e">Details about the navigation failure</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        /// InitializeConfig should implement the commented out code. You should come up with your own, unique password and salt and for added security
+        /// you should implement your own key generator using the IKeyGenerator interface.  
+        /// </summary>
+        /// <returns></returns>
+        protected override async void InitializeConfig()
+        {
+            var config = await SDKManager.InitializeConfigAsync<Config>(new EncryptionSettings(new HmacSHA256KeyGenerator()));
+            config.SaveConfig();
+        }
+
+        /// <summary>
+        /// This returns the root application of your application. Please adjust to match your actual root page if you use something different.
+        /// </summary>
+        /// <returns></returns>
+        protected override Type SetRootApplicationPage()
+        {
+            return typeof(MainPage);
+        }
+    }
+}
---
+++ Salesforce.Sample.SmartSyncExplorer/App.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Windows.UI.Xaml.Navigation;
+using Salesforce.Sample.SmartSyncExplorer.Shared.Pages;
+using Salesforce.SDK.App;
+using Salesforce.SDK.Auth;
+using Salesforce.SDK.Source.Security;
+using Salesforce.SDK.Source.Settings;
+using Windows.ApplicationModel.Activation;
+using Salesforce.Sample.SmartSyncExplorer.Settings;
+
+
+// The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=402347&clcid=0x409
+
+namespace Salesforce.Sample.SmartSyncExplorer
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : SalesforceApplication
+    {
+        /// <summary>
+        ///     Invoked when Navigation to a certain page fails
+        /// </summary>
+        /// <param name="sender">The Frame which failed navigation</param>
+        /// <param name="e">Details about the navigation failure</param>
+        private void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        ///     InitializeConfig should implement the commented out code. You should come up with your own, unique password and
+        ///     salt and for added security
+        ///     you should implement your own key generator using the IKeyGenerator interface.
+        /// </summary>
+        /// <returns></returns>
+        protected override async void InitializeConfig()
+        {
+            var config = await SDKManager.InitializeConfigAsync<Config>(new EncryptionSettings(new HmacSHA256KeyGenerator()));
+            config.SaveConfig();
+        }
+
+        /// <summary>
+        ///     This returns the root application of your application. Please adjust to match your actual root page if you use
+        ///     something different.
+        /// </summary>
+        /// <returns></returns>
+        protected override Type SetRootApplicationPage()
+        {
+            return typeof(MainPage);
+        }
+
+        protected override void OnActivated(IActivatedEventArgs args)
+        {
+            base.OnActivated(args);
+            if (MainPage.ContactsDataModel != null && AccountManager.GetAccount() != null)
+            {
+                MainPage.ContactsDataModel.SyncDownContacts();
+            }
+        }
+    }
+}
