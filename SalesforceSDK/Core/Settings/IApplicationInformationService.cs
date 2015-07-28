7722f2711109c4edaeb0eb607995ddbd8669d6a7
Justin Horst <jhsfdc@outlook.com>
2015-07-28 08:22:09 -0700

    some nit code cleanup

--- SalesforceSDK/Core/Settings/IApplicationInformationService.cs
+++ SalesforceSDK/Core/Settings/IApplicationInformationService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace Core.Settings
 {
@@ -15,6 +11,7 @@ namespace Core.Settings
         Task<string> ReadApplicationFileAsync(string path);
 
         void SaveConfigurationSettings(string config);
+
         string GetConfigurationSettings();
 
         void ClearConfigurationSettings();
